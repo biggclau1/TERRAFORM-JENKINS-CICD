7a7a3b6e84a3658d8db2b6efc2f206447a3285a9
Clauvis T <143439742+biggclau1@users.noreply.github.com>
2025-02-18 13:11:59 -0500

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -16,6 +16,22 @@ resource "aws_s3_bucket_public_access_block" "example" {
   ignore_public_acls      = false
   restrict_public_buckets = false
 }
+resource "aws_s3_bucket_policy" "allow_public_read" {
+  bucket = aws_s3_bucket.mybucket.id
+  policy = <<POLICY
+  {
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Effect": "Allow",
+        "Principal": "*",
+        "Action": "s3:GetObject",
+        "Resource": "arn:aws:s3:::your-bucket-name/*"
+      }
+    ]
+  }
+  POLICY
+}
 resource "aws_s3_bucket_acl" "example" {
   depends_on = [
     aws_s3_bucket_ownership_controls.example,
