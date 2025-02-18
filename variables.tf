8515f2faf950031dd49e537ebae3ce56e14478d6
Clauvis T <143439742+biggclau1@users.noreply.github.com>
2025-02-18 08:39:44 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -17,7 +17,7 @@ variable "ami_id" {
 variable "bucket_name" {
   description = "The name of the S3 bucket to create"
   type        = string
-  default     = "biggclau-terraform-project-cloud"
+  default     = "biggclau-cloud-project"
 }
 
 variable "acl" {
